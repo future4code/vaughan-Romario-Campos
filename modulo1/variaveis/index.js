231e097353c924b9e64f758518c27bc37aee7451
Romario Campos <roriaom@gmail.com>
2021-11-29 23:30:38 -0300

    Exercicios variaveis imcompleto

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,44 @@
+// 1ª Questão:
+
+//let nome;
+//let idade;
+//alert ( `Parte 1: Seu nome é: ${typeof nome} Sua idade é: ${typeof idade}`);
+
+// Impresso tipo undefind pois a não foi declarado nenhum valor.
+
+
+//let nome2 = prompt("Digite o seu nome: ");
+//let idade2 =  prompt("Digite a sua idade");
+//alert ( `Parte 2: Seu nome é: ${typeof nome2} Sua idade é: ${typeof idade2}`);
+
+// Impresso como String, pois a IDE não faz distinção de tipo, necessitando a conversão de um tipo para outro.
+
+//let nome3 = prompt("Digite o seu nome: ");
+//let idade3 =  prompt("Digite a sua idade");
+//alert ( `Parte 3: Olá: ${nome3}, você tem ${idade3} anos`);
+
+// 2ª Questão
+
+//let roupa = prompt("Você está usando uma roupa azul hoje? ");
+//let tempo = prompt("O dia está ensolarado? ");
+//let vinculo =  prompt("É ou já foi aluno da LABENU?");
+//alert ( `Exercicio 2: Você está usando uma roupa azul hoje? ${roupa} O dia está ensolarado? ${tempo} É ou já foi aluno da LABENU? ${vinculo}`);
+
+// 3ª Questão:
+
+let a = 10
+let b = 25
+a = c
+b = a
+c = b
+
+alert ( `Exercicio 3: O novo valor de A é: ${a} O novo valor de B é: ${b}`); //Não deu certo, continuarei tentando!
+
+
+
+
+
+
+
+
+
