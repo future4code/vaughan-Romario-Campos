01614b53c644b3b0518892f938d07b414d2b738a
Romario Campos <roriaom@gmail.com>
2021-12-02 23:54:37 -0300

    exercicios-variaveis-incompleto

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,39 @@
+//1ª Questão 
+
+/*
+a) O que vai ser impresso no console?
+R: 10 e 50
+b) O que aconteceria se retirasse os dois `console.log` e simplesmente invocasse a função
+ `minhaFuncao(2)` e `minhaFuncao(10)`? O que apareceria no console?
+R: Continuaria sendo executado o programa, somente não seria exibido o resultado.
+ */
+
+//2ª Questão 
+/*
+Inconclusivo.
+*/
+
+//3º
+
+//"Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."
+
+/*
+var nome = "caio";
+var idade = 23;
+var cidade = "São Paulo";
+var estudante = "sou";
+
+function alterarNome(nome)
+*/
+
+/*
+function alterarIdade
+function alterarCidade
+function alterarEstudante
+
+Troque o nome, idade, cidade e se é estudante ou não por informações sobre você
+*/
+
+
+
+
