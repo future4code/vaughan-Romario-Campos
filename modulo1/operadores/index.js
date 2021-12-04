3883e74f9ce0b543110280b6ebc0ccdc20f07262
Romario Campos <roriaom@gmail.com>
2021-12-03 23:45:21 -0300

    projeto-lista-js-incompleto

--- modulo1/operadores/index.js
+++ modulo1/operadores/index.js
@@ -100,7 +100,28 @@ console.log ("O segundo numero é divisível pelo primeiro?", divDois);
 
 
 
+function soma(a,b){return a-b;} 
+console.log(soma(10,20));
 
 
+/*
+function multiplica() {
+    return num1 * num2;
+  }
+  console.log(multiplica(10,20));
+*/
 
+/*
+var num1 = 10
+var num2 = 20
+var multiplica = (num1*num2)
 
+console.log(multiplica)
+*/
+/*
+var num1 = 10
+var num2 = 20
+var subtracao = (num2-num1)
+
+console.log(subtracao);
+*/
